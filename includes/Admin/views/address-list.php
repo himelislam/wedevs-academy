ecf80f67ccac95e25d4599d5fe54ec944fef12d4
himelislam <himel7100@gmail.com>
2022-12-07 19:59:44 +0600

    address data fatched and pagination feature added

--- includes/Admin/views/address-list.php
+++ includes/Admin/views/address-list.php
@@ -1,5 +1,15 @@
+<?php require_once( '/xampp/htdocs/wordpress/wp-content/plugins/wedevs-academy/includes/Admin/Address_List.php'); ?>
+
 <div class="wrap">
     <h1 class="wp-heading-inline"><?php _e('Address Book', 'wedevs-academy') ?></h1>
 
     <a class="page-title-action" href="<?php echo admin_url('admin.php?page=wedevs-academy&action=new') ?>"><?php _e('Add New', 'wedevs-academy') ?></a>
+
+    <form action="" method="post">
+        <?php
+        $table = new \WeDevs\Academy\Admin\Address_List();
+        $table->prepare_items();
+        $table->display();
+        ?>
+    </form>
 </div>
--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,11 @@
 <?php
 
-
+/**
+ * Insert address to database
+ *
+ * @param array $args
+ * @return int
+ */
 function wd_ac_insert_address($args = []){
     global $wpdb;
 
@@ -35,4 +40,51 @@ function wd_ac_insert_address($args = []){
     }
 
     return $wpdb->insert_id;
+}
+
+/**
+ * Fetch All Address
+ *
+ * @param array $args
+ * @return array
+ */
+function wd_ac_get_addresses( $args=[] ){
+    global $wpdb;
+
+    $defaults = [
+        'number' => 20,
+        'offset' => 0,
+        'orderby' => 'id',
+        'order' => 'ASC'
+    ];
+
+    $args = wp_parse_args( $args, $defaults );
+
+
+    $sql = $wpdb->prepare(
+        "SELECT * FROM {$wpdb->prefix}ac_addresses 
+        ORDER BY {$args['orderby']} {$args['order']}
+        LIMIT %d",
+         $args['number']
+    );
+
+    // echo $sql;
+
+
+    $items = $wpdb->get_results($sql);
+
+    // var_dump($items);
+
+    return $items;
+}
+
+/**
+ * Get the count of total address
+ *
+ * @return int
+ */
+function wd_ac_address_count(){
+    global $wpdb;
+
+    return (int) $wpdb->get_var("SELECT count(id) FROM {$wpdb->prefix}ac_addresses");
 }
